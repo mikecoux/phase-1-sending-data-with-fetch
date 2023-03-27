efe43133a31ae424fdacf51c555fea0838735dbb
Michael Marcoux <michaelmarcoux99@gmail.com>
2023-03-27 15:18:23 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData (name, email){
+    const submission = {
+        name: name,
+        email: email,
+    }
+
+    return fetch ('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(submission),
+    })
+    .then (res => res.json())
+    .then (data => document.querySelector('body').append(data.id))
+    .catch (error => document.querySelector('body').append(error.message))
+}
